e2a50ce378233de02590ebf294b2e3c7b974fd46
Durnev <shaneshka@inbox.ru>
2020-10-02 16:10:02 +0300

    clean

--- ReportService/ReportService/Settings.cs
+++
@@ -1,9 +0,0 @@
-namespace ReportService.ReportService
-{
-    public static class Settings
-    {
-        public const string EmployeeSalaryUrl = "http://salary.local/api/empcode";
-        public const string EmployeeCodeUrl = "http://buh.local/api/inn";
-        public const string NpgsqlConnection = "Host=192.168.99.100;Username=postgres;Password=1;Database=employee";
-    }
-}
